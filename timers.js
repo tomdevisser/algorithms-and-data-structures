2f2d2b0d70fdd606fc383ad4b349e55d92379348
Thomas de Visser <thomasdevisser@pm.me>
2023-02-28 16:49:38 +0100

    Testing performance using timers

---
+++ timers.js
@@ -0,0 +1,22 @@
+// Testing the performance of several functions to determine which is the most efficient.
+function addUpTo(n) {
+	let total = 0;
+	for (let i = 1; i <= n; i++) {
+		total += i;
+	}
+	return total;
+}
+
+function addUpTo2(n) {
+	return (n * (n + 1)) / 2;
+}
+
+let t1 = performance.now();
+addUpTo(1000000000);
+let t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
+
+let t3 = performance.now();
+addUpTo2(1000000000);
+let t4 = performance.now();
+console.log(`Time Elapsed: ${(t4 - t3) / 1000} seconds.`);
